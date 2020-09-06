202e41fff4012506b915435a1b18db460ea852b6
Maxim <maksfomin@gmail.com>
2020-09-07 01:11:26 +0300

    Create HW 1.1

---
+++ src/main/java/consumer/MyRabbitMQReceiver.java
@@ -0,0 +1,29 @@
+package consumer;
+
+import java.util.Scanner;
+
+public class MyRabbitMQReceiver {
+
+    private final static String FIRST_CH_NAME = "firstCH";
+    private final static String SECOND_CH_NAME = "secondCH";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите название канала: \"firstCH\" или \"secondCH\"");
+        System.out.println("Для смены канала введите: \"/change 'и имя канала'\"");
+        String chanelName = scanner.nextLine();
+        while (true){
+            String msg = scanner.nextLine();
+            if (msg.equals("exit") || msg.equals("quit")) break;
+            if (msg.startsWith("/change ")){
+                chanelName = msg.split(" ", 2)[1];
+                //change chanel
+            }
+            //sending messages
+        }
+        scanner.close();
+    }
+
+}
