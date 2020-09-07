bec4ec6e7fe4bf8b43d367db6941806b22472db9
Maxim <maksfomin@gmail.com>
2020-09-07 13:09:19 +0300

    Create HW 3.1

--- src/main/java/consumer/MyRabbitMQReceiver.java
+++ src/main/java/consumer/MyRabbitMQReceiver.java
@@ -1,29 +1,52 @@
 package consumer;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class MyRabbitMQReceiver {
 
-    private final static String FIRST_CH_NAME = "firstCH";
-    private final static String SECOND_CH_NAME = "secondCH";
+    private final static String PRACTICE = "practice.java";
+    private final static String THEORY = "theory.java";
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, TimeoutException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите название канала: \"firstCH\" или \"secondCH\"");
+        System.out.println("Введите название желаемого канала: \"practice.java\" или \"theory.java\"");
         System.out.println("Для смены канала введите: \"/change 'и имя канала'\"");
         String chanelName = scanner.nextLine();
+
         while (true){
             String msg = scanner.nextLine();
             if (msg.equals("exit") || msg.equals("quit")) break;
             if (msg.startsWith("/change ")){
-                chanelName = msg.split(" ", 2)[1];
-                //change chanel
+                chanelName = msg.split(" ", 2)[1]; // change chanel
             }
-            //sending messages
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(chanelName, BuiltinExchangeType.TOPIC);
+            // String queueName = channel.queueDeclare().getQueue();
+            String queueName = chanelName;
+
+            String routingKey = "";
+            if (chanelName.equals("practice.java")) routingKey = "[practice.java]";
+            if (chanelName.equals("theory.java")) routingKey = "[theory.java]";
+
+            channel.queueBind(queueName, chanelName, routingKey);
+            System.out.println(" [*] Waiting for messages with routing key (" + routingKey + "):");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+            };
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+            });
         }
         scanner.close();
     }
-
 }
--- src/main/java/producer/MyRabbitMQSender.java
+++ src/main/java/producer/MyRabbitMQSender.java
@@ -1,29 +1,55 @@
 package producer;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class MyRabbitMQSender {
 
-    private final static String FIRST_CH_NAME = "firstCH";
-    private final static String SECOND_CH_NAME = "secondCH";
+    private final static String PRACTICE = "practice.java";
+    private final static String THEORY = "theory.java";
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException, TimeoutException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите название канала: \"firstCH\" или \"secondCH\"");
+        System.out.println("Введите название публикуемого канала: \"practice.java\" или \"theory.java\"");
         System.out.println("Для смены канала введите: \"/change 'и имя канала'\"");
         String chanelName = scanner.nextLine();
+
         while (true){
             String msg = scanner.nextLine();
             if (msg.equals("exit") || msg.equals("quit")) break;
             if (msg.startsWith("/change ")){
-                chanelName = msg.split(" ", 2)[1];
-                //change chanel
+                chanelName = msg.split(" ", 2)[1]; // change chanel
             }
-            //sending messages
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            try (Connection connection = factory.newConnection();
+                 Channel channel = connection.createChannel()) {
+                channel.exchangeDeclare(chanelName, BuiltinExchangeType.TOPIC);
+                channel.queueDeclare(chanelName, false, false, false, null);
+
+                String routingKey = "";
+                if (chanelName.equals("practice.java")) routingKey = "[practice.java]";
+                if (chanelName.equals("theory.java")) routingKey = "[theory.java]";
+
+                StringBuilder message = new StringBuilder("message");
+                for (int i = 0; i < 10; i++) {
+                    message.append(" ").append(i);
+                    channel.basicPublish(chanelName, routingKey, null, message.toString().getBytes("UTF-8"));
+                    System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+                    message.delete(0, message.length()).append("message");
+                }
+
+                System.out.println("Введите название публикуемого канала: \"practice.java\" или \"theory.java\"");
+                System.out.println("Для смены канала введите: \"/change 'и имя канала'\"");
+            }
         }
         scanner.close();
     }
-
 }
